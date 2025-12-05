182e786ad924cdf860a70178d19e060dc108845d
Shreyas Nikam <158344330+shreyas-nikam@users.noreply.github.com>
2025-12-05 14:15:55 -0500

    Add application_pages/page_1_home.py

---
+++ application_pages/page_1_home.py
@@ -0,0 +1,9 @@
+
+import streamlit as st
+
+def main():
+    st.markdown(
+        """
+        # Navigating Regulatory Scrutiny: Explaining Credit Decisions with AI
+
+        Welcome, **Quantitative Analyst**! In today's financial landscape, transparency in AI-driven decisions isn
